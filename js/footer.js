49a99a8fa559360a58dbd41a1e6d53ba568ea0b2
lwyuki0524 <y940068@yahoo.com.tw>
2020-03-29 11:14:17 +0800

    網程筆記
    index.html ->首頁

---
+++ js/footer.js
@@ -0,0 +1,6 @@
+document.write("<img class=\"js-back-to-top back-to-top\" title=\"回到頭部\" src=\"pic/top.png\" style=\"width:70px;position:fixed;right:15px;bottom:15px;\">");
+document.write("<footer>");
+document.write("<br><br><br><br><br>");
+document.write("<div style=\"color: white;text-align: center;bottom: 20px;position: relative;\">Copyright © 2019 李宛毓</div>");
+document.write("</footer>");	
+	
---
+++ js/header.js
@@ -0,0 +1,28 @@
+document.write("<header class=\"header\">")
+document.write("<nav>")
+	document.write("<div class=\"menu\">")
+		document.write("<a href=\"index.html\" title=\"首頁\">首頁</a>")
+		document.write("<div class=\"dropdown\">");
+			document.write("<a href=\"#school\" title=\"學業\" class=\"dropbtn\">學業</a>");
+			document.write("<div class=\"dropdown-content\">");
+				document.write("<a style=\"float: none;\" href=\"EEHC.html\">電資與人類文明</a>");
+				document.write("<a style=\"float: none;\" href=\"BE.html\">企業概論</a>");
+				document.write("<a style=\"float: none;\" href=\"BDC.html\">企業資料通訊</a>");
+				document.write("<a style=\"float: none;\" href=\"OS.html\">作業系統</a>");
+				document.write("<a style=\"float: none;\" href=\"Politics.html\">台灣政治與民主</a>");
+				document.write("<a style=\"float: none;\" href=\"WorkCom.html\">楚漢相爭職場競爭力</a>");
+			document.write("</div>");
+		document.write("</div>");
+	document.write("</div>")
+document.write("</nav>")
+document.write("</header>")	
+
+					
+						
+						
+						
+						
+						
+						
+					
+				
